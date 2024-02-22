0274f8c8526fbce0cc59ab431974b7eb433f08f3
ibrosha <ibrosha24@gmail.com>
2024-02-22 19:02:48 +0500

    searchaZadm

--- src/reducers/search/searchred.js
+++ src/reducers/search/searchred.js
@@ -1,10 +1,11 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { getUsers } from '../../pages/search/search'
+import { getUserHistory, getUsers } from '../../pages/search/search'
 
 export const counterSlice = createSlice({
   name: 'searchUsers',
   initialState: {
     data: [],
+    dataHistory: [],
   },
   reducers: {},
   
@@ -17,6 +18,17 @@ export const counterSlice = createSlice({
         state.data = actions.payload
     })
     builder.addCase(getUsers.rejected, (state, actions) => {
+        console.log(error);
+    })
+
+
+    builder.addCase(getUserHistory.pending, (state, actions) => {
+        console.log(1);
+    })
+    builder.addCase(getUserHistory.fulfilled, (state, actions) => {
+        state.dataHistory = actions.payload
+    })
+    builder.addCase(getUserHistory.rejected, (state, actions) => {
         console.log(error);
     })
   }
