2eab4cca8d9e6ba24593e3c94be6fdab35ed13ed
Fazliddin <fazliddinidiev@gmail.com>
2024-02-22 19:03:22 +0500

    Lamar

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,73 +1,108 @@
-import { IconButton } from '@mui/material';
+import { IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import PersonIcon from "@mui/icons-material/Person";
+import ArticleIcon from "@mui/icons-material/Article";
 import AddIcon from "@mui/icons-material/Add";
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+
+import Carousel from "../../assets/icons/Carousel.png"
 
 import Backdrop from "@mui/material/Backdrop";
 import Modal from "@mui/material/Modal";
 import Fade from "@mui/material/Fade";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import "../../App.css"
 
 const style = {
+  position: "absolute",
+  top: "33%",
+  left: "70%",
+  transform: "translate(-50%, -50%)",
+  width: 340,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  borderRadius: "10px",
+  outline:"none",
+  p: 4,
+};
+const styleModal = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 340,
   bgcolor: "background.paper",
   boxShadow: 24,
-  borderRadius:"10px",
+  borderRadius: "10px",
   p: 4,
 };
-
-
 import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import Box from "@mui/material/Box";
 
-import TurnedInNotIcon from '@mui/icons-material/TurnedInNot';
+import TurnedInNotIcon from "@mui/icons-material/TurnedInNot";
 
-import { getProfileById,GetPostByUser } from '../../api/profile/profile';
-import { useDispatch, useSelector } from 'react-redux';
+import { getProfileById, GetPostByUser } from "../../api/profile/profile";
+import { useDispatch, useSelector } from "react-redux";
 
-import { getToken } from '../../utils/token';
+import { destroyToken, getToken } from "../../utils/token";
+import { Link, useNavigate } from "react-router-dom";
 const Profile = () => {
   const [value, setValue] = useState("1");
-  
-  // const [editProfile, setEditProfile] = useState(false);
-  
+
+  const [editProfile, setEditProfile] = useState(false);
+  const handleOpenEditProfile = () => setEditProfile(true);
+  const handleCloseEditProfile = () => setEditProfile(false);
+
   const [menuProfile, setMenuProfile] = useState(false);
   const handleOpenProfile = () => setMenuProfile(true);
   const handleCloseProfile = () => setMenuProfile(false);
 
-  const dispatch = useDispatch()
-  const userProfile = useSelector((store) => store.profile.userProfile)
-  const postProfile = useSelector((store) => store.profile.postProfile)
+  const [imageProfile, setImageProfile] = useState("");
+
+  const [modalProfile, setModalProfile] = useState(false);
+  const handleOpenModalProfile = (elem) => (
+    setImageProfile(elem),
+    setModalProfile(true)
+  )
+  const handleCloseModalProfile = () => setModalProfile(false);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userProfile = useSelector((store) => store.profile.userProfile);
+  const postUser = useSelector((store) => store.profile.postUser);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  console.log(userProfile.userName) 
+  function logOut() {
+    navigate("/");
+    destroyToken("access_token");
+  }
+
+  console.log(userProfile.userName);
+  // console.log(postProfile)
 
   console.log(getToken());
-useEffect(() => {
-  dispatch(getProfileById(getToken().sid));
-  dispatch(GetPostByUser(getToken().sid));
-}, []);
+  useEffect(() => {
+    dispatch(getProfileById(getToken().sid));
+    dispatch(GetPostByUser(getToken().sid));
+  }, [dispatch]);
 
   return (
     <div className="p-[60px] pr-[200px] ">
       <div className="flex justify-between">
-        <div className="w-[28%]">
+        <div className="w-[30%]">
           <img
-            className="w-[82%] rounded-full  h-[25vh] object-cover"
+            className="w-[82%] rounded-full  h-[27vh]"
             src={
               length == 0
-                ? "https://tse3.mm.bing.net/th?id=OIP.s3RJ4bcuEf9d2BBzCCB_0wHaHa&pid=Api&P=0&h=220"
+                ? "https://tse4.mm.bing.net/th?id=OIP.jixXH_Els1MXBRmKFdMQPAHaHa&pid=Api&P=0&h=220"
                 : userProfile.avatar
             }
             alt=""
@@ -78,8 +113,11 @@ useEffect(() => {
             <div>
               <h1 className="text-[22px] font-[700]">{userProfile.userName}</h1>
             </div>
-            <div className="flex items-center gap-[20px]  h-[50px]">
-              <button className="w-[120px] text-[18px] h-[45px] bg-[whitesmoke] rounded-xl">
+            <div className="flex items-center gap-[20px]  h-[50px] ">
+              <button
+                // onClick={() => handleOpenEditProfile()}
+                className="w-[120px]  text-[18px] h-[45px] bg-[whitesmoke] rounded-xl"
+              >
                 Edit profile
               </button>
               <button className="w-[120px] text-[18px] h-[45px] bg-[whitesmoke] rounded-xl ">
@@ -91,24 +129,24 @@ useEffect(() => {
             </div>
           </div>
           <div className="flex justify-between w-[70%] p-[0px] items-center mt-[20px] mb-[15px]">
-            <div className="flex w-[32%]">
-              <h1 className="text-[20px] text-[gray] ">
-                <span className="text-[20px] font-[700] text-[black] pr-[5px]">
+            <div className="flex w-[32%] hover:border-[1px] hover:bg-[whitesmoke] hover:duration-700 cursor-pointer  text-center p-[5px] rounded-xl">
+              <h1 className="text-[20px] text-[gray] text-center">
+                <span className="text-[20px] font-[700] text-[black] pr-[5px] pl-[10px]">
                   {userProfile.postCount}
                 </span>
                 posts
               </h1>
             </div>
-            <div className="w-[32%] flex ">
-              <h1 className="text-[20px] text-[gray] ">
+            <div className="w-[32%] flex hover:border-[1px] hover:bg-[whitesmoke] hover:duration-700 cursor-pointer  text-center p-[5px] rounded-xl pl-[10px]">
+              <h1 className="text-[20px] text-[gray] text-center ">
                 <span className="text-[20px] font-[700] text-[black] pr-[5px]">
                   {userProfile.subscribersCount}
                 </span>
                 follower
               </h1>
             </div>
-            <div className="w-[32%] flex  ">
-              <h1 className="text-[20px] text-[gray] ">
+            <div className="w-[32%] flex hover:border-[1px] hover:bg-[whitesmoke] hover:duration-700 cursor-pointer   text-center p-[5px] rounded-xl pl-[10px]">
+              <h1 className="text-[20px] text-[gray]">
                 <span className="text-[20px] font-[700] text-[black] pr-[5px]">
                   {userProfile.subscriptionsCount}
                 </span>
@@ -117,7 +155,9 @@ useEffect(() => {
             </div>
           </div>
           <div className="w-[70%] mt-[20px] mb-[15px]">
-            <h1 className="text-[30px] font-[700]">{userProfile.fullName}</h1>
+            <h1 className="text-[30px] font-[500] text-[#323131]">
+              {userProfile.fullName}
+            </h1>
           </div>
         </div>
       </div>
@@ -136,28 +176,135 @@ useEffect(() => {
           <h1 className="text-center text-[18px]">New</h1>
         </div>
       </div>
-      <div className="w-[500px] m-[auto]">
+      <div className=" m-[auto]">
         <TabContext value={value}>
-          <Box sx={{ borderBottom: 0, borderColor: "divider" }}>
+          <Box
+            sx={{
+              borderBottom: 0,
+              borderColor: "divider",
+              width: "100%",
+            }}
+          >
             <TabList onChange={handleChange} aria-label="lab API tabs example">
-              <Tab label="posts" value="1" />
-              <Tab label="Saved" value="2" />
-              <Tab label="Tagged" value="3" />
+              <Tab
+                icon={<ArticleIcon sx={{ width: "40px", height: "40px" }} />}
+                sx={{
+                  marginLeft: "250px",
+                  fontSize: "18px",
+                  fontWeight: "600",
+                }}
+                label="posts"
+                value="1"
+              />
+              <Tab
+                sx={{
+                  fontSize: "18px",
+                  fontWeight: "600",
+                  marginLeft: "30px",
+                }}
+                icon={
+                  <BookmarkBorderIcon
+                    sx={{
+                      width: "37px",
+                      height: "37px",
+                    }}
+                  />
+                }
+                label="Saved"
+                value="2"
+              />
+              <Tab
+                sx={{
+                  fontSize: "18px",
+                  fontWeight: "600",
+                  marginLeft: "30px",
+                }}
+                icon={
+                  <PersonIcon
+                    sx={{
+                      width: "37px",
+                      height: "37px",
+                    }}
+                  />
+                }
+                label="Tagged"
+                value="3"
+              />
             </TabList>
           </Box>
-          <TabPanel value="1">
-            {/* {
-              postProfile.map((elem) => {
+          <TabPanel sx={{ width: "100%" }} value="1">
+            <div className="flex justify-between items-center flex-wrap gap-[10px]">
+              {postUser?.map((elem) => {
                 return (
-                  <div>
-                    <img src={elem.images} alt="" />
+                  <div
+                    onClick={() => handleOpenModalProfile(elem)}
+                    className="w-[31%] mt-[10px] h-[30vh] bg-[whitesmoke] rounded-xl   "
+                  >
+                    {elem.images.map((image, index) => (
+                      <img
+                        className="w-[100%] h-[100%] rounded-xl"
+                        src={`${import.meta.env.VITE_APP_FILES_URL}/${
+                          elem.images[0]
+                        }`}
+                        alt=""
+                      />
+                    ))}
                   </div>
-                )
-              })
-            } */}
+                );
+              })}
+            </div>
           </TabPanel>
-          <TabPanel value="2">Item Two</TabPanel>
-          <TabPanel value="3">Item Three</TabPanel>
+          <TabPanel sx={{ width: "100%" }} value="2">
+            <div>
+              <div className="flex items-center justify-between">
+                <h1 className="text-[20px] font-[500] text-[#3b3b3b]">
+                  Only you can see what you've saved
+                </h1>
+                <Button sx={{ fontSize: "18px" }} variant="text">
+                  <span className="text-[22px]">+</span> New collection
+                </Button>
+              </div>
+              <div className="flex justify-between flex-wrap w-[600px] h-[500px] mt-[10px] overflow-auto">
+                {postUser?.map((elem) => {
+                  return (
+                    <div
+                      onClick={() => handleOpenModalProfile(elem)}
+                      className="w-[31%] mt-[10px] h-[30vh] bg-[whitesmoke] rounded-xl hover:w-[32%] hover:h-[31vh] hover:duration-700 "
+                    >
+                      {elem.images.map((image, index) => (
+                        <img
+                          className="w-[100%] h-[80%] rounded-xl"
+                          src={`${import.meta.env.VITE_APP_FILES_URL}/${
+                            elem.images[0]
+                          }`}
+                          alt=""
+                        />
+                      ))}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel sx={{ width: "100%" }} value="3">
+            <div className="flex justify-between items-center flex-wrap">
+              {postUser?.map((elem) => {
+                return (
+                  <div onClick={() => handleOpenModalProfile(elem)} className="w-[31%] h-[30vh] mt-[10px] bg-[whitesmoke] rounded-xl  ">
+                    {elem.images.map((image, index) => (
+                      <img
+                        className="w-[100%] h-[100%] rounded-xl"
+                        src={`${import.meta.env.VITE_APP_FILES_URL}/${
+                          elem.images[0]
+                        }`}
+                        alt=""
+                      />
+                    ))}
+                  </div>
+                );
+              })}
+            </div>
+          </TabPanel>
         </TabContext>
         <Modal
           aria-labelledby="transition-modal-title"
@@ -174,13 +321,68 @@ useEffect(() => {
         >
           <Fade in={menuProfile}>
             <Box sx={style}>
-              
+              <div>
+                <h1 className="p-[15px] font-[500] text-[19px] rounded-xl hover:bg-[whitesmoke] hover:duration-500">
+                  QR code
+                </h1>
+                <h1 className="p-[15px] font-[500] text-[19px] rounded-xl hover:bg-[whitesmoke] hover:duration-500">
+                  Notification
+                </h1>
+                <h1 className="p-[15px] font-[500] text-[19px] rounded-xl hover:bg-[whitesmoke] hover:duration-500">
+                  Settings and privacy
+                </h1>
+                <h1
+                  onClick={() => logOut()}
+                  className="p-[15px] font-[500] text-[19px] rounded-xl hover:bg-[whitesmoke] hover:duration-500 text-red-500"
+                >
+                  Log out
+                </h1>
+              </div>
+            </Box>
+          </Fade>
+        </Modal>
+
+        {/* <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={editProfile}
+          onClose={handleCloseEditProfile}
+          closeAfterTransition
+          slots={{ backdrop: Backdrop }}
+          slotProps={{
+            backdrop: {
+              timeout: 500,
+            },
+          }}
+        >
+          <Fade in={editProfile}>
+            <Box sx={style}>wnfnjwnjnj</Box>
+          </Fade>
+        </Modal> */}
+
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={modalProfile}
+          onClose={handleCloseModalProfile}
+          closeAfterTransition
+          slots={{ backdrop: Backdrop }}
+          slotProps={{
+            backdrop: {
+              timeout: 500,
+            },
+          }}
+        >
+          <Fade in={modalProfile}>
+            <Box sx={styleModal}>
+              <div>
+                <img src="" alt="" />
+              </div>
             </Box>
           </Fade>
         </Modal>
       </div>
     </div>
   );
-}
-
-export default Profile
+};
+export default Profile;
